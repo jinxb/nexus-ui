6e0ad6dbb54bc2763fd08b563a0d4c9914a978a5
jinxb <1659340302@qq.com>
2023-04-11 14:14:33 +0800

    build

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,30 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'packages/index.ts'),
+      name: 'Nxui',
+      formats: ['es'],
+      fileName: () => `nxui.min.js`
+    },
+    rollupOptions: {
+      external: ['vue', 'element-plus', 'vxe-table'],
+      output: {
+        globals: {
+          vue: 'vue'
+        }
+      }
+    },
+    minify: false
   }
 })
