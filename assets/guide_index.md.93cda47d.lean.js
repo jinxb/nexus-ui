62c7aaa86eb6105f605059de026d5c25b99b31de
jinxb <jinxb@users.noreply.github.com>
2023-05-17 09:30:38 +0000

    deploy: 80986cc5590d561cc8ba9e0c7779021d1e5050a9

---
+++ assets/guide_index.md.93cda47d.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,o as a,c as n,R as e}from"./chunks/framework.c44288e8.js";const _=JSON.parse('{"title":"安装","description":"","frontmatter":{},"headers":[],"relativePath":"guide/index.md","lastUpdated":1684315781000}'),l={name:"guide/index.md"},p=e("",8),o=[p];function t(c,r,i,d,C,y){return a(),n("div",null,o)}const u=s(l,[["render",t]]);export{_ as __pageData,u as default};
